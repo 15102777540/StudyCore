d650efd88f7aac4dd5a448311a6bfa252da60a63
xuchunyang <1973271032@qq.com>
2020-10-26 22:18:58 +0800

    2020-10-26 22

--- StudyCore.Api/StudyCore.Api/Startup.cs
+++ StudyCore.Api/StudyCore.Api/Startup.cs
@@ -1,7 +1,10 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -11,6 +14,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.PlatformAbstractions;
+using Microsoft.OpenApi.Models;
+using StudyCore.Model.Auth;
 using StudyCore.Repository;
 
 namespace StudyCore.Api
@@ -27,23 +33,57 @@ namespace StudyCore.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            var appSettingsSection = Configuration.GetSection("AppSettings");
+            services.Configure<AppAuthenticationSettings>(appSettingsSection);
+            // JWT
+            var appSettings = appSettingsSection.Get<AppAuthenticationSettings>();
+            services.AddJwtBearerAuthentication(appSettings);
+            services.AddAutoMapper(typeof(Startup));
             services.AddControllers().AddNewtonsoftJson();
 
             //Add-Migration InitialCreate 创建迁移
             //Update-Database 更新数据库
             //Script-Migration 生成 SQL 脚本
             //Remove-Migration 删除迁移
+            //b => b.MigrationsAssembly("StudyCore.Api") 很关键
             services.AddDbContext<StudyDbContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"),b => b.MigrationsAssembly("StudyCore.Api")));
+
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+
+                var basePath = PlatformServices.Default.Application.ApplicationBasePath; //var basePath = AppDomain.CurrentDomain.BaseDirectory;
+                
+                var xmlModelPath = Path.Combine(basePath, "StudyCore.Api.xml");
+                c.IncludeXmlComments(xmlModelPath);
+                //这个就是Model层的xml文件名                                           
+                //Core.Admin.webapi.xml是我的项目生成XML文档的后缀名,具体的以你项目为主
+                var xmlPath = Path.Combine(basePath, "StudyCore.Model.xml"); 
+                //第二个参数为true的话则控制器上的注释也会显示(默认false)
+                c.IncludeXmlComments(xmlPath, true);
+            });
+
+            // 注入日志
+            services.AddLogging(config =>
+            {
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "Log4net.config");
+                config.AddLog4Net(path);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env,ILoggerFactory loggerFactory)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseStaticFiles();
+            app.UseFileServer();
 
             app.UseHttpsRedirection();
 
@@ -54,6 +94,12 @@ namespace StudyCore.Api
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
             });
         }
     }
